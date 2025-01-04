c4fc894f4219967e12060385d7766297a9a99432
Andy Sit <andysit173@gmail.com>
2025-01-03 20:56:50 -0500

    a note for postmark

--- src/email_facade.py
+++ src/email_facade.py
@@ -1,8 +1,10 @@
 
 ## this file class handles the emailing, response for verification, recaps, and wahtnot.
 
-
-
-# class Email:
-
+# class Email:\
 #https://fastapi-users.github.io/fastapi-users/12.1/usage/routes/#verify-router
+# can also use postmark, free 100 emails/month which is really good!
+
+
+
+
