96f2e7cd1252446a0acf440861171dec2f6c0e12
Andy Dev <dev@andys-air.home>
2025-01-08 15:15:45 -0500

    qr code filled

--- src/util.py
+++ src/util.py
@@ -1,8 +1,16 @@
 
-
-
+import pyqrcode
+import io
 
 #create randomizer id...
-def gen_qr_code(link : str):
-  pass
+def next_bin_id():
+    pass
 
+
+#look into this function since buffer may not work intendedly with an async backend
+def gen_qr_code(link : str):
+    qr_code = pyqrcode.create(link)
+    buffer = io.BytesIO()
+    qr_code.png(buffer, scale=5)
+    buffer.seek(0)
+    return buffer
