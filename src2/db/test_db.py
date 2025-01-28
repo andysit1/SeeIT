805aec1a907d23cd53aacf0ee3ff82e61f8f1ca5
Andy Sit <andysit173@gmail.com>
2025-01-28 09:39:19 -0500

    model to response query added

---
+++ src2/db/test_db.py
@@ -0,0 +1,18 @@
+from DBManager import DatabaseManager
+from UserModels import User
+
+
+if __name__ == "__main__":
+    # i think we should make this a singleton and have this in the init run time.
+    # ask chatgpt after.
+    # Example usage:
+    database_url = "sqlite:///test.db"
+    db_manager = DatabaseManager(database_url)
+    # Add a new user
+    new_user = User(username="Alice2", password="alicepass", email="alice2@example.com")
+    # db_manager.add(new_user)
+
+
+    
+    user_response = db_manager.query_first(User, {"username": "Alice2"})
+    print(user_response)
