ea1c51fce2b1602e6548cdc7ed8716e1f5897983
Andy Sit <andysit173@gmail.com>
2025-02-05 23:13:32 -0500

    changed naming

--- src2/repo/Repository.py
+++ src2/repo/Repository.py
@@ -1,64 +1,49 @@
-from db.DBManager import DatabaseManager
+from src2.db.DBManager import DatabaseManager, User, Bin
+
 
 #for now i'll keep the repo just a single repo instead of branching into user, groups... for simple
 # i think i can get db_manager in crud and just call the models in crud operations for clearner impl.
+
+
+#think of repository as doing the interactions...
+
 class CrudOperations:
+    def __init__(self):
+      self.db_manager : DatabaseManager = DatabaseManager("sqlite:///test.db")
+
         #crud functions
-    def create_user(self):
-        pass
+    def create_model(self):
+        #think of requirements before we create the user...
+        # new_user = User()
+        self.db_manager.add()
 
-    def read_user(self):
-        pass
+    def update_model(self):
+        self.db_manager.update()
 
-    def update_user(self):
-        pass
+    def delete_model(self):
+        self.db_manager.delete()
 
-    def delete_user(self):
-        pass
 
+
+        
 
 # create a user manager which handles the management system for all user
 class UserManager(CrudOperations):
-    def __init__(self):
-      self.db_manager : DatabaseManager = DatabaseManager()
-
-
     #boolean oper.
     def is_user_exist_by_username(self, username: str) -> bool:
-        """
-        Check if a user with the given username already exists in the database.
-
-        :param email: The username to check.
-        :return: True if the user exists, False otherwise.
-        """
-        # session = SessionLocal()
-        # try:
-        #     # Query the database for a user with the given email
-        #     user = session.query(User).filter(User.username == username).first()
-        #     return user is not None
-        # finally:
-        #     session.close()
-        pass
+        user_response = self.db_manager.query_first(User, {"username": username})
+        return bool(user_response)
 
     def is_user_exist_by_email(self, email: str) -> bool:
-        pass
-        """
-        Check if a user with the given email already exists in the database.
-
-        :param email: The email to check.
-        :return: True if the user exists, False otherwise.
-        """
-        # session = SessionLocal()
-        # try:
-        #     # Query the database for a user with the given email
-        #     user = session.query(User).filter(User.email == email).first()
-        #     return user is not None
-        # finally:
-        #     session.close()
+        user_response = self.db_manager.query_first(User, {"email": email})
+        return bool(user_response)
 
     #more specific functions to the site
     def get_user_using_binid(self, binid: int):
-        pass
+        bin_response = self.db_manager.query_first(Bin, {"bin_id": binid})
+        userid = bin_response.user_id
+        return self.db_manager.query_first(User, {"id": userid})
+        # pass
 
 
 # create a user manager which handles the management system for all user
