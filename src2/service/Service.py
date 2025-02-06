95b5a14470fe007671f62414aa9a3bde2d755886
Andy Sit <andysit173@gmail.com>
2025-02-05 23:12:27 -0500

    init service files for org, not used  yet

--- src2/service/Service.py
+++ src2/service/Service.py
@@ -2,18 +2,37 @@
 from repo.Repository import UserManager, GroupManager #serves formatted data...
 
 """
+  How codes are associated with an organization
+
+
+
   Services
-    user
+    user -> easy per use pr code
       text/media
       basic filter for bad text
 
       types of impact?
       location impact?
 
-    group
-      custom way top sep. qr codes?
+    group -> how do we sep an organization codes?
+      we want people in an organization to have codes
+      we want codes to be spread per code
+
+      location -> place
+      email -> funding
+      not every funding is in the same quantity
+      funding per child
+
+
+
+       custom way top sep. qr codes?
+
+
+
 
 """
+
+
 
 
 class BaseService:
@@ -21,10 +40,28 @@ class BaseService:
     def has_permission(self, obj, permission: str) -> bool:
         return permission in obj.permissions
 
+    #profanity checker
+    def is_bad():
+       pass
+
+    #given binid over userid i think
+    def add_media_to_bin(self, binid: int):
+       pass
+
+    def add_media_to_user(self, userid: int):
+       pass
+
+    def pull_media(self, binid):
+       pass
+
 class UserService(BaseService):
   def __init__(self):
      super().__init__()
      self.user_manager : UserManager = UserManager()
+
+   
+
+
     # pass  # User-specific logic
 
   def add_media_to_user(self, userid: int):
---
+++ src2/service/orgs/org_util.py
@@ -0,0 +1,23 @@
+
+"""
+  how webpage codes are associated with users... ()
+  Org : example
+    LondonBoyandClass
+      LBGCD_{code}
+
+"""
+class Organization:
+  def __init__(self):
+    self.name = ""
+
+class PG_Organization(Organization):
+  def __init__(self):
+    super().__init__()
+
+
+
+
+
+
+
+
