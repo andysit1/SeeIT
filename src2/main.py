7d14ab8c2874e9df415828cb216004a3e0d2e3dd
Andy Sit <andysit173@gmail.com>
2025-02-05 23:13:01 -0500

    command to remember to run fastapi

--- src2/main.py
+++ src2/main.py
@@ -1,7 +1,23 @@
 from fastapi import FastAPI
 
+from src2.db.DBManager import DatabaseManager
+from src2.db.UserModels import User, UserResponse
+
 app = FastAPI()
+database_url = "sqlite:///test.db"
+db_manager = DatabaseManager(database_url)
+
+#uvicorn src2.main:app --reload -> can run server like this so that it knows we're in the src2 "dir"...
+
+
+
 
 @app.get("/")
-async def root():
-    return {"message": "Hello World"}
+async def root():    # db_manager.add(new_user)
+    user_response = db_manager.query_first(User, {"username": "Alice2"})
+    print(user_response)
+    return user_response
+
+
+
+
